94cff0bc7b94a55d494933049f22ef3e19d61fdd
Shaunak-Chauthai <shaunakchauthai8@gmail.com>
2024-01-02 15:05:08 +0530

    Created Django Project with signup and login task3

--- social_book/social_book/urls.py
+++ social_book/social_book/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from users import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.SignupPage,name='signup'),
+    path('login/',views.LoginPage,name='login'),
+    path('home/',views.HomePage,name='home'),
+    path('logout/',views.LogoutPage,name='logout'),
+
+
+    
 ]
